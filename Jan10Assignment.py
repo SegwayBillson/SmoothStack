3f83005f0717c6ec0d51ddded371938457ceea38
Doug Woods <42622082+SegwayBillson@users.noreply.github.com>
2022-01-11 14:52:26 -0800

    Updated Jan10 Assignment

--- Jan10Assignment.py
+++ Jan10Assignment.py
@@ -1,5 +1,28 @@
 import math
 
+import math
+
+print(50+50)
+
+print(100-10)
+
+#Throws an error
+#print(30+*6)
+
+print(6^6)
+
+print(6**6)
+
+print(6+6+6+6+6+6)
+
+
+def interest(p, r, l):
+    r = (r/100)/12
+    return math.ceil(p/((((1+r)**l)-1)/(r*(1+r)**l)))
+
+print(interest(800000,6,103))
+print(interest(30000,6,5*12))
+
 def interest(p, r, l):
     r = (r/100)/12
     return math.ceil(p/((((1+r)**l)-1)/(r*(1+r)**l)))
